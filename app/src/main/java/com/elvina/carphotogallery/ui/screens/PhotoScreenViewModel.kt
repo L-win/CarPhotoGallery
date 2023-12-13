50ba7ae1a4b3b2af7c15ab8b5739d4b96f282deb
Elvin Agha <33497735+L-win@users.noreply.github.com>
2023-12-13 17:54:08 +0400

    Add getPhoto method and viewmodel factory to PhotoScreenViewModel

--- app/src/main/java/com/elvina/carphotogallery/ui/screens/PhotoScreenViewModel.kt
+++ app/src/main/java/com/elvina/carphotogallery/ui/screens/PhotoScreenViewModel.kt
@@ -1,17 +1,59 @@
 package com.elvina.carphotogallery.ui.screens
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
-import com.elvina.carphotogallery.data.CarCollectionRepository
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.elvina.carphotogallery.CarPhotoGalleryApplication
+import com.elvina.carphotogallery.data.CarPhotoRepository
 import com.elvina.carphotogallery.model.Photo
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import java.io.IOException
 
 sealed interface PhotoScreenUiState {
-    data class Success(val photos: List<Photo>) : HomeScreenUiState
-    object Error : HomeScreenUiState
-    object Loading : HomeScreenUiState
+    data class Success(val photo: Photo) : PhotoScreenUiState
+    object Error : PhotoScreenUiState
+    object Loading : PhotoScreenUiState
 }
 
 class PhotoScreenViewModel(
-    private val repository: CarCollectionRepository
+    private val repository: CarPhotoRepository
 ) : ViewModel() {
 
+    var photoScreenUiState: PhotoScreenUiState by mutableStateOf(PhotoScreenUiState.Loading)
+        private set
+
+    init {
+        getCarPhoto()
+    }
+
+    fun getCarPhoto() {
+        viewModelScope.launch {
+            photoScreenUiState = PhotoScreenUiState.Loading
+            photoScreenUiState = try {
+                //TODO: pass photo id
+                PhotoScreenUiState.Success(repository.getPhoto(id = "123"))
+            } catch (e: IOException) {
+                PhotoScreenUiState.Error
+            } catch (e: HttpException) {
+                PhotoScreenUiState.Error
+            }
+        }
+    }
+
+    companion object {
+        val Factory: ViewModelProvider.Factory = viewModelFactory {
+            initializer {
+                val application = (this[APPLICATION_KEY] as CarPhotoGalleryApplication)
+                val carPhotoRepository = application.container.carPhotoRepository
+                PhotoScreenViewModel(repository = carPhotoRepository)
+            }
+        }
+    }
 }
